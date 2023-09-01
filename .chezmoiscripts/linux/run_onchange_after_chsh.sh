ff0673a2f84b8698fcf37296100228f82ba70e92
Alexander Büscher <alibue@hotmail.de>
2023-09-01 10:21:37 +0200

    Adjust linux shell change script

--- .chezmoiscripts/linux/run_onchange_after_chsh.sh
+++ .chezmoiscripts/linux/run_onchange_after_chsh.sh
@@ -2,4 +2,19 @@
 
 set -eufo pipefail
 
-chsh -s /usr/bin/zsh
+if [ "$EUID" -eq 0 ]; then
+    # User is root, change shell to zsh
+    user_shell=$(getent passwd $SUDO_USER | cut -d: -f7)
+    if [ "$user_shell" != "/usr/bin/zsh" ]; then
+        chsh -s /usr/bin/zsh $SUDO_USER
+    fi
+else
+    # User is not root, change shell to zsh only if password is correct
+    if sudo chsh -s /usr/bin/zsh $USER; then
+        echo "Shell changed successfully."
+    else
+        echo "Incorrect password or unable to change the shell."
+    fi
+fi
+
+
