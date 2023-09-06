0e9b3f6f7e237e6c3a5121d8a4d235335cf5e240
Alexander Büscher <alibue@hotmail.de>
2023-09-06 22:25:43 +0200

    Fix zsh not being immediately active after shell change

--- home/.chezmoiscripts/linux/run_onchange_after_chsh.sh
+++ home/.chezmoiscripts/linux/run_onchange_after_chsh.sh
@@ -2,17 +2,13 @@
 
 set -eufo pipefail
 
-if [ "$EUID" -eq 0 ]; then
-    user_shell=$(getent passwd $SUDO_USER | cut -d: -f7)
-    if [ "$user_shell" != "/usr/bin/zsh" ]; then
-        chsh -s /usr/bin/zsh $SUDO_USER
-    fi
+if sudo chsh -s /usr/bin/zsh; then
+    echo "Shell changed successfully. Please relog"
 else
-    if sudo chsh -s /usr/bin/zsh $USER; then
-        echo "Shell changed successfully."
-    else
-        echo "Incorrect password or unable to change the shell."
-    fi
+    echo "Incorrect password or unable to change the shell."
 fi
+
+# start a new zsh session
+exec /bin/zsh
 
 
