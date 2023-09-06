32bca7d6b8a12e5e93024527e2811502c587b041
Alexander Büscher <alibue@hotmail.de>
2023-09-07 00:14:21 +0200

    Fix chsh script

--- home/.chezmoiscripts/linux/run_onchange_after_chsh.sh
+++ home/.chezmoiscripts/linux/run_onchange_after_chsh.sh
@@ -2,8 +2,8 @@
 
 set -eufo pipefail
 
-if sudo chsh -s /usr/bin/zsh; then
-    echo "Shell changed successfully. Please relog"
+if chsh -s /usr/bin/zsh; then
+    echo "Shell changed successfully. Please log out and log back in to use Zsh."
 else
     echo "Incorrect password or unable to change the shell."
 fi
