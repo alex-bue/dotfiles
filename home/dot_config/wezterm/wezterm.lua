2e89e721fb66a3f758c3e672a4ee6f02ff5b7ef1
Alexander Büscher <alibue@hotmail.de>
2023-09-14 18:34:03 +0200

    Add amethyst dotfile for tiling wm on macos

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require 'wezterm'
 
 local cfg_appearance = require 'cfg_appearance'
 local cfg_fonts = require 'cfg_fonts'
+local cfg_keybinds = require 'cfg_keybinds'
 
 -- This table will hold the configuration.
 local config = {}
@@ -16,6 +17,7 @@ end
 -- This is where you actually apply your config choices
 cfg_appearance.apply_to_config(config)
 cfg_fonts.apply_to_config(config)
+cfg_keybinds.apply_to_config(config)
 
 -- and finally, return the configuration to wezterm
 return config
