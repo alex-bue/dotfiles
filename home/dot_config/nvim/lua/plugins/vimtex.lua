132af5a3c99e045725332d32bc26c9c03b819509
Alexander Büscher <alibue@hotmail.de>
2024-03-03 22:32:56 +0100

    feat: add obsidian plugin for neovim, small changes to configs

--- home/dot_config/nvim/lua/plugins/vimtex.lua
+++ home/dot_config/nvim/lua/plugins/vimtex.lua
@@ -4,6 +4,7 @@ return {
     vim.g.vimtex_mappings_prefix = 'l'  -- Change the local leader for vimtex
     vim.g.vimtex_view_method = 'skim'
     vim.g.vimtex_compiler_method = 'latexmk'
+    vim.g.vimtex_quickfix_open_on_warning = 0
   end,
 }
 
