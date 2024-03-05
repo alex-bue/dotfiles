203e2d2111f197651b1fd8104e3253c6a899ff4c
Alexander Büscher <alibue@hotmail.de>
2024-03-06 00:37:02 +0100

    feat: adjust obsidian plugin options and set keymaps

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -8,6 +8,34 @@ vim.keymap.del("n", "<leader>l")
 vim.keymap.set("n", "<leader>Ll", "<cmd>Lazy<cr>", { desc = "Lazy" })
 vim.keymap.set("n", "<leader>Lc", function() Util.news.changelog() end, { desc = "LazyVim Changelog" })
 
+-- obsidian.nvim
+vim.keymap.set("n", "<leader>op", "<cmd>ObsidianPasteImg<CR>", { desc = "Paste Image in Obsidian" })
+
+-- vimtex
+vim.keymap.set("n", "<leader>li", "<plug>(vimtex-info)", { desc = "View Info" })
+-- vim.keymap.set("n", "<leader>lI", "<plug>(vimtex-info-full)", { desc = "View Full Info" })
+vim.keymap.set("n", "<leader>lt", "<plug>(vimtex-toc-open)", { desc = "Open Table of Contents" })
+vim.keymap.set("n", "<leader>lT", "<plug>(vimtex-toc-toggle)", { desc = "Toggle Table of Contents" })
+vim.keymap.set("n", "<leader>lq", "<plug>(vimtex-log)", { desc = "View Log" })
+vim.keymap.set("n", "<leader>lv", "<plug>(vimtex-view)", { desc = "View Compiled Document" })
+vim.keymap.set("n", "<leader>lr", "<plug>(vimtex-reverse-search)", { desc = "Reverse Search" })
+vim.keymap.set("n", "<leader>ll", "<plug>(vimtex-compile)", { desc = "Compile" })
+-- vim.keymap.set("n", "<leader>lL", "<plug>(vimtex-compile-selected)", { desc = "Compile Selected" })
+vim.keymap.set("n", "<leader>lk", "<plug>(vimtex-stop)", { desc = "Stop Compilation" })
+-- vim.keymap.set("n", "<leader>lK", "<plug>(vimtex-stop-all)", { desc = "Stop All Compilation" })
+vim.keymap.set("n", "<leader>le", "<plug>(vimtex-errors)", { desc = "View Errors" })
+vim.keymap.set("n", "<leader>lo", "<plug>(vimtex-compile-output)", { desc = "View Compile Output" })
+vim.keymap.set("n", "<leader>lg", "<plug>(vimtex-status)", { desc = "View Status" })
+-- vim.keymap.set("n", "<leader>lG", "<plug>(vimtex-status-all)", { desc = "View Status (All)" })
+vim.keymap.set("n", "<leader>lc", "<plug>(vimtex-clean)", { desc = "Clean" })
+vim.keymap.set("n", "<leader>lC", "<plug>(vimtex-clean-full)", { desc = "Clean Full" })
+-- vim.keymap.set("n", "<leader>lm", "<plug>(vimtex-imaps-list)", { desc = "List of Maps" })
+-- vim.keymap.set("n", "<leader>lx", "<plug>(vimtex-reload)", { desc = "Reload" })
+-- vim.keymap.set("n", "<leader>lX", "<plug>(vimtex-reload-state)", { desc = "Reload State" })
+vim.keymap.set("n", "<leader>ls", "<plug>(vimtex-toggle-main)", { desc = "Toggle Main" })
+vim.keymap.set("n", "<leader>la", "<plug>(vimtex-context-menu)", { desc = "Context Menu" })
+
+
 -- to adjust which-key configuration
 require("config/which-key")
 
--- home/dot_config/nvim/lua/plugins/obsidian.lua
+++ home/dot_config/nvim/lua/plugins/obsidian.lua
@@ -1,7 +1,7 @@
 return {
   "epwalsh/obsidian.nvim",
   version = "*",  -- recommended, use latest release instead of latest commit
-  -- lazy = true,
+  lazy = true,
   ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   -- event = {
@@ -24,16 +24,53 @@ return {
       },
     },
 
+
     disable_frontmatter = true,
 
+    -- Set default notes directory
     notes_subdir = "01-zettelkasten",
     new_notes_location = "notes_subdir",
+
+    -- Option for ObsidianNew
+    note_id_func = function(title)
+      if title == nil or title == '' then
+        error("File name is required.")
+      end
+      return title
+    end,
+
     templates = {
       subdir = "_templates"
     },
 
+    preferred_link_style = "wiki",
+
+    -- Prefix with timestamp
+    image_name_func = function()
+      return string.format("%s-", os.date("%Y%m%d%H%M%S"))
+    end,
+
+		-- Options for ObsidianPasteImg
     attachments = {
+			-- Default folder
       img_folder = "_attachments",
+
+			-- Output string format
+      img_text_func = function(client, path)
+        local link_path_str
+        local vault_relative_path = client:vault_relative_path(path)
+
+        if vault_relative_path ~= nil then
+          -- Convert the `obsidian.Path` object to a string
+          link_path_str = tostring(vault_relative_path)
+        else
+          -- If `vault_relative_path` is `nil`, convert the original `path` to a string
+          link_path_str = tostring(path)
+        end
+
+        local display_name = vim.fs.basename(link_path_str)
+        return string.format("![[%s]]", display_name)
+      end,
     },
   },
 }
