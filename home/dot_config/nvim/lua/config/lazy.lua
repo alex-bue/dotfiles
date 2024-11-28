24d948c5a5a277090765800d7005b1daa2c999f6
Alexander Büscher <alibue@hotmail.de>
2024-11-28 22:21:51 +0100

    Updates kickstarter neovim

--- home/dot_config/nvim/lua/config/lazy.lua
+++ home/dot_config/nvim/lua/config/lazy.lua
@@ -34,6 +34,7 @@ require("lazy").setup({
 	require("plugins.todo-comments"),
 	require("plugins.mini"),
 	require("plugins.treesitter"),
+	require("plugins.obsidian"),
 
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
