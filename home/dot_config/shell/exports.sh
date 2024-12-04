67b8f589cc981662f81e050a9c2d640232fe49d5
Alexander Büscher <alibue@hotmail.de>
2024-12-04 16:42:37 +0100

    more refactoring and templating for shared configs

---
+++ home/dot_config/shell/exports.sh
@@ -0,0 +1,9 @@
+# Exports
+
+# Editor
+export NVIM_APPNAME="lazyvim"
+export EDITOR="nvim"
+export VISUAL="nvim"
+
+# Starship prompt config location
+export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml" # Starship config
