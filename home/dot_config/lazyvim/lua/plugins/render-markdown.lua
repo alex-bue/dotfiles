1c467f1a7d25cce1cb526b9ccbc2314f1043bf3e
Alexander Büscher <alibue@hotmail.de>
2024-12-05 16:37:15 +0100

    Updates to nvim config

---
+++ home/dot_config/lazyvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,10 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    heading = {
+      sign = false,
+      position = false,
+      icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+    },
+  },
+}
